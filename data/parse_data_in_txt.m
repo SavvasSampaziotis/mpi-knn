6975310b4d171bfe9fe734f38c6331a6adad9f1c
Savvas Sampaziotis <savvas456pentium@gmail.com>
2017-12-09 19:21:13 +0200

    Data formatting

---
+++ data/parse_data_in_txt.m
@@ -0,0 +1,36 @@
+
+f = 1;
+datasets = {"mnist_train", "mnist_train_svd"};
+
+fileName = datasets{f};
+
+load( strcat('./raw_data/',fileName ,'.mat'));
+
+[N, D] = size(train_X);
+% For testing purposes only
+N = 100;
+D = 3;
+
+n=1:N;
+d = 1:D;
+
+joinData = [train_X(n,d), train_labels(n)];
+header = [N,D];
+
+outputFile = strcat('./formatted_data/',fileName, '.txt');
+
+dlmwrite(outputFile, header, ...
+    'delimiter','\t', ...
+    'newline','pc');
+dlmwrite(outputFile, joinData , ...
+    'delimiter','\t', ...
+    'newline','pc', ...
+    '-append');
+
+% FIle Format:
+% N D  # Header       # D+1
+% x11 x12 x13 ... x1D label1  # Main Data:  [datapoint label]
+% x21 x22 x23 ... x2D label2
+% x31 x32 x33 ... x3D label3
+%   ...
+% xN1 xN2 xN3 ... xND labelN
